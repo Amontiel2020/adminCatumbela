3e0a70e1717b638c457dd9957a7cdf256a2ab418
Amontiel2020 <adan.montiel@gmail.com>
2020-12-08 11:17:45 +0000

    cambios de teste

--- app/Models/Estudante.php
+++ app/Models/Estudante.php
@@ -5,13 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Trabalhador extends Model
+class Estudante extends Model
 {
     use HasFactory;
-
-       protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
 }
